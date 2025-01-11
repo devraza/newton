56845981af5ca1145caaa1cddc92c360f9b3d325
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-01-11 13:40:21 +0000

    solutions: 1807A

---
+++ solutions/rust/1807A.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    let mut t = String::new();
+    io::stdin().read_line(&mut t).unwrap();
+    
+    for _ in 1..=t.trim().parse::<i16>().unwrap() {
+        let mut n = String::new();
+        io::stdin().read_line(&mut n).unwrap();
+
+        let array = n.trim().split_whitespace().collect::<Vec<&str>>();
+    
+        let mut int_array: Vec<i8> = vec![];
+        for i in array {
+            int_array.push(i.parse::<i8>().unwrap());
+        }
+
+        if int_array[0] + int_array[1] == int_array[2] {
+            println!("+");
+        } else {
+            println!("-");
+        }
+    }
+}
