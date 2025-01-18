2b2a80b9445c4f96f00bb342fa2c74fc9b862fc6
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-01-18 00:22:46 +0000

    solutions: 200B

---
+++ solutions/rust/200B.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+
+    let n = s.trim().parse::<u64>().unwrap();
+
+    let mut p = String::new();
+    io::stdin().read_line(&mut p).unwrap();
+
+    let drinks = p.trim().split_whitespace().map(|c| c.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+
+    let conc = drinks.iter().sum::<u64>() as f64 / n as f64;
+
+    println!("{}", conc);
+}
