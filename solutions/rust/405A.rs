fc0b52910dabe1395d4c794caa921cd8889c95d0
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-01-20 19:16:18 +0000

    solutions: 405A

---
+++ solutions/rust/405A.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+fn main() {
+    let mut n = String::new();
+    io::stdin().read_line(&mut n).unwrap();
+
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+
+    let mut initial = s.trim().split_whitespace().map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+
+    initial.sort();
+
+    let output = initial.iter().map(|d| d.to_string()).collect::<Vec<String>>();
+
+    println!("{}", output.join(" "));
+}
