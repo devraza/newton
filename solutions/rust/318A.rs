e5b5366785d7f1c5c3585b0e43854da1c0a72792
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-01-20 11:40:16 +0000

    solutions: 318A

---
+++ solutions/rust/318A.rs
@@ -0,0 +1,20 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+
+    let input = s.trim().split_whitespace().map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+    let n = input[0];
+    let k = input[1];
+
+    if k > (((n as f64/ 2.).ceil() as u64)) {
+        if n % 2 == 0 {
+            println!("{}", (2*(k-(n/2))))
+        } else {
+            println!("{}", (2*(k-((n+1)/2))))
+        }
+    } else {
+        println!("{}", (2*k-1))
+    }
+}
