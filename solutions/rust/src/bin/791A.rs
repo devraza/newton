d5331cdee9672db26df3b656026151e888b42f5a
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-01-20 22:21:29 +0000

    solutions: 791A

---
+++ solutions/rust/src/bin/791A.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+
+    let a = s.trim().split_whitespace().map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+    let mut l = a[0];
+    let mut b = a[1];
+
+    let mut y = 0;
+    while l <= b {
+        y += 1;
+        b *= 2;
+        l *= 3;
+    }
+
+    println!("{}", y);
+}
