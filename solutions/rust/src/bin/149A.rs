9ebefaa2d1c7a85463f12454b430d5d1cda108df
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-01-29 21:29:44 +0000

    solutions: 149A

---
+++ solutions/rust/src/bin/149A.rs
@@ -0,0 +1,33 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+
+    let n = s.trim().parse::<u64>().unwrap();
+
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    
+    let mut a: Vec<u64> = s.split_whitespace().map(|d| d.parse::<u64>().unwrap()).collect();
+
+    a.sort();
+    a.reverse();
+   
+    let mut count = 0;
+    let mut sum = 0;
+
+    if a.iter().sum::<u64>() < n {
+        println!("-1");
+        std::process::exit(0);
+    }
+
+    for i in a {
+        if sum < n {
+            sum += i;
+            count += 1;
+        }
+    }
+
+    println!("{}", count);
+}
