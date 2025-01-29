08a50a8d87e48ff658c663d8866e4fd451ad8268
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-01-29 12:13:29 +0000

    solutions: 1327A

---
+++ solutions/rust/src/bin/1327A.rs
@@ -0,0 +1,24 @@
+use std::io;
+
+fn main() {
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).unwrap();
+    let t: u64 = s.trim().parse().unwrap();
+
+    for _ in 0..t {
+        let mut s = String::new();
+        io::stdin().read_line(&mut s).unwrap();
+
+        let a: Vec<u64> = s.split_whitespace().map(|d| d.parse::<u64>().unwrap()).collect();
+    
+        if a[0] % 2 == a[1] % 2  {
+            if a[1].pow(2) > a[0] {
+                println!("NO");
+            } else {
+                println!("YES");
+            }
+        } else {
+            println!("NO");
+        }
+    }
+}
