d102c020fce5bd5a7ce5bad5ee5d4d6fcf3b42cc
Muhammad Nauman Raza <devraza.hazard643@slmail.me>
2025-04-04 09:52:08 +0100

    solutions: 1742A

---
+++ solutions/python/1742A.py
@@ -0,0 +1,9 @@
+t = int(input())
+
+for _ in range(t):
+    a = list(map(int, input().split(' ')))
+    
+    if a[0] == a[1]+a[2] or a[1] == a[0]+a[2] or a[2] == a[1]+a[0]:
+        print("YES")
+    else:
+        print("NO")
